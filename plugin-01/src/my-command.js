4c42ff2c9c6444b37f0e8248dd24dfc9f5c2b3d6
Manhart, John <manhartjohn@gmail.com>
2017-12-29 12:03:40 -0600

    Changing some structure and getting another test to run.
    🎶 Finger - Ty Segall

---
+++ plugin-01/src/my-command.js
@@ -0,0 +1,10 @@
+export default function (context) {
+  const selectedLayers = context.selection
+  const selectedCount = selectedLayers.length
+
+  if (selectedCount === 0) {
+    context.document.showMessage('No layers are selected.')
+  } else {
+    context.document.showMessage(`${selectedCount} layers selected.`)
+  }
+}
